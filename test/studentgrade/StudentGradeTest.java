ba584f0aea291033f327dd7fa8729c8a9e89f284
0320 <0320@LAPTOP-JOUD>
2026-04-27 00:25:55 +0300

    updated StudentGradeTest from JUnit 3.8.1 to JUnit 4.13.2

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,14 +1,11 @@
 package studentgrade;
 
-import junit.framework.TestCase;
-
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
+import org.junit.Test;
+import static org.junit.Assert.*;
+/* JUnit 4.13.2 */
+public class StudentGradeTest {
     
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
-
+     @Test
     public void testGetGrade95() {
         System.out.println("getGrade 95");
         int mark = 95;
@@ -16,7 +13,8 @@ public class StudentGradeTest extends TestCase {
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
     }
-
+    
+     @Test
     public void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
@@ -25,8 +23,9 @@ public class StudentGradeTest extends TestCase {
         assertEquals(expResult, result);
     }
     
+     @Test
     public void testGetGrade75() {
-        System.out.println("getGrade 85");
+        System.out.println("getGrade 75");
         int mark = 75;
         String expResult = "F";
         String result = StudentGrade.getGrade(mark);
